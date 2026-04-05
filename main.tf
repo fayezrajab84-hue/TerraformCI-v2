39be37d6462cb0a63c0fa09d34d0113943ea94b4
Mohammed Rajab <fayez.rajab84@gmail.com>
2026-04-05 22:07:03 +0800

    update-tfc-details

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ terraform {
 
   # Terraform Cloud backend — workspace settings managed in TFC UI
   cloud {
-    organization = "your-tfc-organization"
+    organization = "cybervaultscool"
     workspaces {
-      name = "your-workspace-name"
+      name = "TerraformCI"
     }
   }
 }
